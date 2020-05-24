1b36a4587eb286e9b618c6d2428630c30c15cc0c
SaraCremonesi <saracrem00@gmail.com>
2020-05-24 11:28:46 +0200

    aggiustamenti

--- js/script.js
+++ js/script.js
@@ -5,14 +5,13 @@ var buttonAnnulla = document.getElementById('redo');
 var formName = document.getElementById('form-name');
 var formSurname = document.getElementById('form-surname');
 var formKm = document.getElementById('form-chilometers');
-var numberKm = parseInt(formKm);
 var formAge = document.getElementById('form-age');
 
 // Declaration ticket details
 var ticketName = document.getElementById('ticket-name');
 var ticketSurname = document.getElementById('ticket-surname');
 var ticketPrice = document.getElementById('price');
-var ticketOffer = document.getElementById('offer');
+var ticketOfferType = document.getElementById('offer');
 var ticketCarriage = document.getElementById('carriage');
 var ticketCode = document.getElementById('code');
 var ticket = document.getElementById('ticket');
@@ -25,30 +24,28 @@ buttonGenera.addEventListener('click',
   function () {
     ticketName.innerHTML = formName.value;
     ticketSurname.innerHTML = formSurname.value;
-    var ageValue = formAge.value;
     var numberKm = parseInt(formKm.value);
 
-    if (isNaN(numberKm) || (formName.value).length == 0 || (formSurname.value).length == 0 || ageValue == 'empty') {
-      error.className = 'visible';
+    if ((isNaN(numberKm) || numberKm < 1) || (formName.value).length == 0 || (formSurname.value).length == 0 || formAge.value == 'empty') {
+      error.className = 'show';
     } else {
       error.className = 'hidden';
-      ticket.className = 'visible';
+      ticket.className = 'show';
 
       var priceKm = numberKm * 0.21;
-      var offer = 'Prezzo intero';
+      ticketOfferType.innerHTML = 'Prezzo intero';
 
-      if (ageValue == 'minore') {
+      if (formAge.value == 'minore') {
         priceKm = priceKm - (priceKm * 20 / 100);
-        offer = 'Sconto ragazzi';
-      } else if (ageValue == 'anziano') {
+        ticketOfferType.innerHTML = 'Sconto ragazzi';
+      } else if (formAge.value == 'anziano') {
         priceKm = priceKm - (priceKm * 40 / 100);
-        offer = 'Sconto over 65';
+        ticketOfferType.innerHTML = 'Sconto over 65';
       }
 
       ticketPrice.innerHTML = priceKm.toFixed(2) + '€';
-      ticketOffer.innerHTML = offer;
-      ticketCarriage.innerHTML = Math.floor(Math.random() * 10 ) + 1;
-      ticketCode.innerHTML = Math.floor(Math.random() * 100000) + 1;
+      ticketCarriage.innerHTML = Math.ceil(Math.random() * 10 ) + 1;
+      ticketCode.innerHTML = Math.ceil(Math.random() * 100000) + 1;
     }
   }
 );
@@ -59,11 +56,10 @@ buttonAnnulla.addEventListener('click',
      ticketName.innerHTML = '';
      formSurname.value = '';
      ticketSurname.innerHTML = '';
-     formKm.value = '';
+     numberKm = '';
      formAge.value = 'empty';
 
      ticket.className = 'hidden';
      error.className = 'hidden';
-     hide.className = 'hidden';
    }
  );
